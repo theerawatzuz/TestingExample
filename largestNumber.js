a522d98d2e73a02bd628006c752412c8e5fb7576
TheerawatNB <bankt21@gmail.com>
2024-02-28 13:53:54 +0700

    CreatelargestNumber

--- largestNumber.js
+++ largestNumber.js
@@ -16,28 +16,10 @@ async function getInput() {
     //Convert input data to array 
     const numbersArray = inputData.trim().split(' ').map(Number);
 
-    //Convert Array data to custom sort
-    const numberCustomSort = numbersArray.slice().sort((a,b) => {
-        //Sort odd number first
-        if(a % 2 !== 0 && b % 2 !== 0){
-            return a - b;
-        }
-        //Sort even number after odd numbers
-        else if (a % 2 === 0 && b % 2 === 0){
-            return a - b;
-        }
-        //Keep odd numbers before even numbers
-        else if (a % 2 !== 0 && b % 2 === 0){
-            return -1;
-        }
-        else {
-            return 1;
-        }
-    });
+  
 
         //Message output 
         console.log('Input', numbersArray);
-        console.log('Output', numberCustomSort);
 
 }
 
