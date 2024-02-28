05910655f50f1090ae06e9c110e5d5ce6e34ec4a
TheerawatNB <bankt21@gmail.com>
2024-02-28 15:16:24 +0700

    AddFnCheckPrimeAndLoop

--- sumOfAllPrimes.js
+++ sumOfAllPrimes.js
@@ -18,8 +18,26 @@ async function getInput() {
 
 
     //Message output target
-    console.log('Input', targetData);
-    console.log('Output', targetNumber);
+    console.log('Input', targetNumber);
+    
+    for (let i = 2; i <= targetNumber; i++){
+        if (isPrime(i)){
+            console.log(i);
+        }
+    }
+}
+    //function check prime number?
+    function isPrime(number){
+        if (number <= 1){
+            return false;
+        }
+        for (let i = 2; i <= Math.sqrt(number); i++){
+            if (number % i === 0){
+                return false;
+            }
+        }
+        return true;
+  
 
 
 }
